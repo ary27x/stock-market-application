4a3f1b9154fbf22274db81e7fffd39aee2b21206
ary27x <ary27x@gmail.com>
2024-04-05 09:03:24 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
-
+import Header from './components/Header';
+import DayIntervalChart from './components/DayIntervalChart';
 function App() {
+  const [name , setName] = useState("IBM")
+  const [data , setData] = useState([])
+  const [dayIntervalData , setDayIntervalData] = useState({})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name = {name} setName = {setName} setData = {setData} setDayIntervalData = {setDayIntervalData}/>
+      
+      <DayIntervalChart dayIntervalData = {dayIntervalData}id = "first" />
+      <DayIntervalChart dayIntervalData = {dayIntervalData} id = "second"/>
+
     </div>
   );
 }
